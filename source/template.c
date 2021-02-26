30e60d3683fc993b8a26f154ccd30b7c28502378
Ethan Crooks <8bitbanana.ec@gmail.com>
2021-02-26 17:57:30 +0000

    Example arrow sprite

--- source/template.c
+++ source/template.c
@@ -40,6 +40,34 @@ Color mixcolor(Color a, Color b, u8 mix) {
 	return a;
 }
 
+void Copy8x8TileArea(void* source, void* dest, u8 width, u8 height) {
+	const u32 tile_word_size = 0x8;
+	const u32 row_size = 0x400;
+
+	while (height-- > 0) {
+		CpuFastSet(
+			source, 
+			dest,
+			COPY32 | (tile_word_size * width)
+		);
+		source = source + (tile_word_size * width * 4);
+		dest += row_size;
+	}
+}
+
+void WriteToOAM(const struct oam_norot* data, u8 index) {
+
+	u32 upper = ((u32*)data)[0];
+	u32 lower = ((u32*)data)[1];
+	
+	OAM[index*2] = upper;
+	OAM[index*2 + 1] = lower;
+}
+
+void WriteToOAM_Rot(const struct oam_rot* data, u8 index) {
+	WriteToOAM((const struct oam_norot*)data, index);
+}
+
 const u8 hello_world_indexes[] = {1,2,3,3,4,9,5,4,6,3,7,8};
 
 int main(void) {
@@ -57,6 +85,8 @@ int main(void) {
 		((u8*)BG_COLORS)[i] = font_pal_bin[i];
 	}
 
+	const Color red = {12,0,0};
+	((Color*)BG_COLORS)[0] = red;
 
 	struct bgcnt* const bgcnt = (struct bgcnt*)BGCTRL;
 
@@ -75,6 +105,33 @@ int main(void) {
 	} 
 	
 	CpuFastSet(font_img_bin, (u16*)VRAM,(font_img_bin_size/4) | COPY32);
+	
+	const struct oam_norot empty_sprite = {
+		.disabled = true
+	};
+
+	// Clear OAM
+	WriteToOAM(&empty_sprite, 0);
+	CpuFastSet(OAM, OAM, FILL | COPY32 | (256));
+
+	const struct oam_norot sprite_base = {
+		.shape = OBJ_SHAPE_SQUARE,
+		.size = OBJ_SIZE_16,
+		.x = 100,
+		.y = 100,
+		.tile = 0
+	};
+
+	WriteToOAM(&sprite_base, 0);
+
+	u16* obj_tile_addr = (u16*)0x6010000;
+
+	Copy8x8TileArea((u16*)arrow_img_bin, obj_tile_addr, 2, 2);
+	//CpuFastSet(arrow_img_bin, obj_tile_addr, (arrow_img_bin_size/4) | COPY32);
+
+	for (u8 i=0; i<arrow_pal_bin_size; i++) {
+		((u8*)OBJ_COLORS)[i] = arrow_pal_bin[i];
+	}
 
 	while (1) {
 		// Code goes here!
